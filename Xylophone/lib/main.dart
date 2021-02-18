f789dcba2507161763608a0581458d2e21dcc7f8
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2021-02-18 23:21:38 +1030

    Xylophone complete

--- Xylophone/lib/main.dart
+++ Xylophone/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,11 +6,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static AudioCache player = AudioCache();
+  Expanded xbutton({Color color, int num}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$num.wav');
+        },
+        child: Text(''),
+      ),
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              xbutton(color: Colors.purple[900], num: 1),
+              xbutton(color: Colors.blue[900], num: 2),
+              xbutton(color: Colors.blue[300], num: 3),
+              xbutton(color: Colors.green, num: 4),
+              xbutton(color: Colors.yellow, num: 5),
+              xbutton(color: Colors.orange, num: 6),
+              xbutton(color: Colors.red, num: 7),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
